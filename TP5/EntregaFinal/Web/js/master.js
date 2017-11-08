ee6f6a0ac10dc10da2cffece1a0717e2c7006ef0
Silvina Muñoz <silmunoz22@gmail.com>
2017-11-07 21:34:10 -0300

    prototipo en alta carousel

--- TP5/EntregaFinal/Web/js/master.js
+++ TP5/EntregaFinal/Web/js/master.js
@@ -14,40 +14,40 @@ $(document).ready(function() {
 // });
 
 // $("#lista-img").hide();
-
-$("#tweet").click(function(event) {
-  event.preventDefault();
-  $("#lista-img").show();
-
-  let params = {
-    q: $("#tweet-search").val(),
-    count: 30
-  };
-
-  cb.__call(
-    "search_tweets",
-    params,
-    function (reply) {
-      var photosArray = [];
-
-      for (var i = 0; i < reply.statuses.length; i++) {
-        let tweet = reply.statuses[i];
-        if(tweet.extended_entities && tweet.extended_entities.media[0].type == "photo") {
-          console.log(tweet);
-          let urlImg = tweet.extended_entities.media[0].media_url_https;
-          if(!photosArray.includes(tweet.extended_entities.media[0].media_url_https)){
-            if(tweet.extended_entities.media.length > 0){
-              console.log("entro al 2do if");
-            photosArray.push(urlImg);
-            }
-            photosArray.push(urlImg);
-            $("#lista-img").append('<li class="col-md-6"><img src="'+urlImg+'" class="img-tweet" width="300" height="300" alt=""></li>');
-          }
-        }
-      }
-    },
-  );
-});
+//
+// $("#tweet").click(function(event) {
+//   event.preventDefault();
+//   $("#lista-img").show();
+//
+//   let params = {
+//     q: $("#tweet-search").val(),
+//     count: 30
+//   };
+//
+//   cb.__call(
+//     "search_tweets",
+//     params,
+//     function (reply) {
+//       var photosArray = [];
+//
+//       for (var i = 0; i < reply.statuses.length; i++) {
+//         let tweet = reply.statuses[i];
+//         if(tweet.extended_entities && tweet.extended_entities.media[0].type == "photo") {
+//           console.log(tweet);
+//           let urlImg = tweet.extended_entities.media[0].media_url_https;
+//           if(!photosArray.includes(tweet.extended_entities.media[0].media_url_https)){
+//             if(tweet.extended_entities.media.length > 0){
+//               console.log("entro al 2do if");
+//             photosArray.push(urlImg);
+//             }
+//             photosArray.push(urlImg);
+//             $("#lista-img").append('<li class="col-md-6"><img src="'+urlImg+'" class="img-tweet" width="300" height="300" alt=""></li>');
+//           }
+//         }
+//       }
+//     },
+//   );
+// });
 
 
 
